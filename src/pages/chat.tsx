fcf750411026362491d718f213aa322722dec896
Hein Htet Aung <hhaung5142@gmail.com>
2023-04-11 02:50:31 +0800

    page setup improvement

--- src/pages/chat.tsx
+++ src/pages/chat.tsx
@@ -1,39 +1,12 @@
 import * as Mui from "@mui/material";
 
-// const useStyles = makeStyles({
-//     table: {
-//       minWidth: 650,
-//     },
-//     chatSection: {
-//       width: '100%',
-//       height: '80vh'
-//     },
-//     headBG: {
-//         backgroundColor: '#e0e0e0'
-//     },
-//     borderRight500: {
-//         borderRight: '1px solid #e0e0e0'
-//     },
-//     messageArea: {
-//       height: '70vh',
-//       overflowY: 'auto'
-//     }
-//   });
-
 const Chat = () => {
   return (
     <div>
-      <Mui.Grid container>
-        <Mui.Grid item xs={12}>
-          <Mui.Typography variant="h5" className="header-message">
-            Chat
-          </Mui.Typography>
-        </Mui.Grid>
-      </Mui.Grid>
       <Mui.Grid
         container
         component={Mui.Paper}
-        sx={{ width: "100%", height: "80vh" }}
+        sx={{ width: "100%", height: "100vh" }}
       >
         <Mui.Grid item xs={3}>
           <Mui.List>
@@ -79,24 +52,30 @@ const Chat = () => {
               </Mui.ListItemIcon>
               <Mui.ListItemText primary="Alice">Alice</Mui.ListItemText>
             </Mui.ListItem>
-            <Mui.ListItem button key="CindyBaker">
-              <Mui.ListItemIcon>
-                <Mui.Avatar
-                  alt="Cindy Baker"
-                  src="https://material-ui.com/static/images/avatar/2.jpg"
-                />
-              </Mui.ListItemIcon>
-              <Mui.ListItemText primary="Cindy Baker">
-                Cindy Baker
-              </Mui.ListItemText>
-            </Mui.ListItem>
           </Mui.List>
         </Mui.Grid>
         <Mui.Grid item xs={9}>
           <Mui.List>
+            <Mui.ListItem button key="Alice">
+              <Mui.ListItemIcon>
+                <Mui.Avatar
+                  alt="Alice"
+                  src="https://material-ui.com/static/images/avatar/3.jpg"
+                />
+              </Mui.ListItemIcon>
+              <Mui.ListItemText primary="Alice">Alice</Mui.ListItemText>
+              <Mui.ListItemText
+                sx={{ textAlign: "right" }}
+                secondary="Last seen 4mins ago"
+              ></Mui.ListItemText>
+            </Mui.ListItem>
+          </Mui.List>
+          <Mui.Divider />
+          <Mui.List>
             <Mui.ListItem key="1">
               <Mui.Grid container>
                 <Mui.Grid item xs={12}>
+                  <Mui.ListItemText primary="User 1"></Mui.ListItemText>
                   <Mui.ListItemText primary="Hey man, What's up ?"></Mui.ListItemText>
                 </Mui.Grid>
                 <Mui.Grid item xs={12}>
@@ -107,20 +86,11 @@ const Chat = () => {
             <Mui.ListItem key="2">
               <Mui.Grid container>
                 <Mui.Grid item xs={12}>
-                  <Mui.ListItemText primary="Hey, Iam Good! What about you ?"></Mui.ListItemText>
+                  <Mui.ListItemText primary="User 2"></Mui.ListItemText>
+                  <Mui.ListItemText primary="Hey, I am Good! What about you ?"></Mui.ListItemText>
                 </Mui.Grid>
                 <Mui.Grid item xs={12}>
                   <Mui.ListItemText secondary="09:31"></Mui.ListItemText>
-                </Mui.Grid>
-              </Mui.Grid>
-            </Mui.ListItem>
-            <Mui.ListItem key="3">
-              <Mui.Grid container>
-                <Mui.Grid item xs={12}>
-                  <Mui.ListItemText primary="Cool. i am good, let's catch up!"></Mui.ListItemText>
-                </Mui.Grid>
-                <Mui.Grid item xs={12}>
-                  <Mui.ListItemText secondary="10:30"></Mui.ListItemText>
                 </Mui.Grid>
               </Mui.Grid>
             </Mui.ListItem>
